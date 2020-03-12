8ff993e5c36b44aa1a48204e8b4373188cbb3fef
Dominik Dorosz <dominikdorosz93@gmail.com>
2020-03-12 16:49:28 -0400

    Create testing.rb

---
+++ testing.rb
@@ -0,0 +1,10 @@
+Given /^the user has an account$/ do
+  @user = User.create(name: "Example User", email: "user@example.com",
+                      password: "foobar", password_confirmation: "foobar")
+end
+
+When /^the user submits valid signin information$/ do
+  fill_in "Email",    with: @user.email
+  fill_in "Password", with: @user.password
+  click_button "Sign in"
+end
